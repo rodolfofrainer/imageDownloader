d5d4d1f41886619e80047039abcceeafe4056cd1
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-05-08 13:44:37 +0100

    fix bug where boilerplate image was downloaded

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-from bs4 import BeautifulSoup
 import requests
+import threading
 
-links = []
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 \
+        (Macintosh; Intel Mac OS X 10_10_1) \
+        AppleWebKit/537.36 (KHTML, like Gecko) \
+            Chrome/39.0.2171.95 Safari/537.36'}
+
+
+def download_image(url, index):
+    page = requests.get(url.strip(), headers=HEADERS)
+    with open(f'images/{index}.jpg', 'wb') as file:
+        file.write(page.content)
+    print(f'Downloaded {url}')
+
 
 with open('combisteel_images.txt', 'r') as file:
-    links.append(file.readlines())
+    links = file.readlines()
 
-for link in links[0]:
-    scrapeLink = link
-    page = requests.get(scrapeLink)
-    # soup = BeautifulSoup(page.content, 'html.parser')
-    print(page)
+threads = []
+for index, link in enumerate(links[:5]):
+    thread = threading.Thread(target=download_image, args=(link, index))
+    threads.append(thread)
+    thread.start()
+
+for thread in threads:
+    thread.join()
