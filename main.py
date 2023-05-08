bcc3ebbf0c3fc66f7005928f1939f0420adf2cc9
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-05-08 13:59:41 +0100

    create user gui and encapsulated class

--- main.py
+++ main.py
@@ -1,28 +1,7 @@
-import requests
-import threading
+from imageDowloaderGUI import ImageDownloaderGUI
 
-HEADERS = {
-    'User-Agent': 'Mozilla/5.0 \
-        (Macintosh; Intel Mac OS X 10_10_1) \
-        AppleWebKit/537.36 (KHTML, like Gecko) \
-            Chrome/39.0.2171.95 Safari/537.36'}
+import tkinter as tk
 
-
-def download_image(url, index):
-    page = requests.get(url.strip(), headers=HEADERS)
-    with open(f'images/{index}.jpg', 'wb') as file:
-        file.write(page.content)
-    print(f'Downloaded {url}')
-
-
-with open('combisteel_images.txt', 'r') as file:
-    links = file.readlines()
-
-threads = []
-for index, link in enumerate(links[:5]):
-    thread = threading.Thread(target=download_image, args=(link, index))
-    threads.append(thread)
-    thread.start()
-
-for thread in threads:
-    thread.join()
+root = tk.Tk()
+app = ImageDownloaderGUI(root)
+app.mainloop()
