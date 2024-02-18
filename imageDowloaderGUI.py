68f5eb9bdbf2a072e81f8b84888471cc33d46627
Rodolfo F <rodolfo_frainer@hotmail.com>
2024-02-18 13:58:32 +0100

    refactor code

--- imageDowloaderGUI.py
+++ imageDowloaderGUI.py
@@ -4,11 +4,9 @@ import threading
 import tkinter as tk
 from tkinter import filedialog
 
-
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
 }
-
 
 class ImageDownloaderGUI(tk.Frame):
     def __init__(self, master=None):
